fea296ca7165a85e5346be4d2c6c9568d36121a2
WL <alakamande@gmail.com>
2024-02-18 11:19:44 -0800

    new changes
    changed handling and speed, currently set with keyboard controls, arrowkeys

--- scripts/bicycle.gd
+++ scripts/bicycle.gd
@@ -2,11 +2,11 @@ extends Area3D
 
 @export var bikers = []
 @export var max_velocity = 100
-@export var friction = 0.25
-@export var handling = 5000
+@export var friction = 0.5
+@export var handling = 1000
 @export var biker_offset = 2.5
 @export var speedFactor = 0.025
-var current_velocity = 0
+var current_velocity = 0.0
 var current_lean = 0
 var current_dir = 0
 var biker_res = preload("res://gameobjects/biker.tscn")
@@ -31,15 +31,15 @@ func _process(delta):
 		crash()
 	current_lean = 0
 	for biker in bikers:
-		current_velocity += biker.current_force
+		current_velocity += biker.current_force/bikers.size()
 		current_lean += biker.current_lean
 	#current_lean = current_lean/bikers.size()
 	#if(current_velocity > max_velocity):
 	#	current_velocity = max_velocity
 	#print(bikers[0].current_velocity)
 	position += current_velocity * forward_vector * delta * speedFactor
-	rotation.y += current_lean * delta * (current_velocity/handling)
-	current_velocity -= ((current_velocity * friction) + 1) * delta
+	rotation.y += deg_to_rad(current_lean * delta * (current_velocity/handling))
+	current_velocity -= ((current_velocity * friction) + 250) * delta
 	if(current_velocity < 0):
 		current_velocity = 0
 		
@@ -90,6 +90,6 @@ func crash():
 		biker.top_level = true
 		biker.freeze = false
 		var randvect = Vector3.ZERO
-		randvect.x = randf()
-		randvect.z = randf()
-		biker.apply_impulse((current_velocity * forward_vector * randvect)*100)
+		#randvect.x = randf()
+		#randvect.z = randf()
+		biker.apply_impulse((current_velocity * forward_vector)*0.1)
--- scripts/biker.gd
+++ scripts/biker.gd
@@ -1,27 +1,43 @@
 extends RigidBody3D
 
-@export var max_lean = 10
-@export var max_force = 5
+@export var max_lean = 65
+@export var max_force = 15
 var mouse_velocity = Vector2.ZERO
 var current_lean = 0
 var current_force = 0
+var rider
+@export var lean_factor = 0.4
+@export var kb_lean_factor = 500
+@export var kb_speed = 500
 
 # This is assigned a unique peer ID on multiplayer connection
 var peer_id = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	rider = get_node("rider")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	if multiplayer.get_unique_id() != peer_id:
 		return
 	
-	var mouse_pos = get_viewport().get_mouse_position()
-	var mouse_vel = get_viewport().get_visible_rect().size/2 - mouse_pos
+	
+	var input_vel = Vector2.ZERO
+	#var mouse_pos = get_viewport().get_mouse_position()
+	#var mouse_vel = get_viewport().get_visible_rect().size/2 - mouse_pos
 	#Input.warp_mouse(get_viewport().get_visible_rect().size/2)
-	current_lean = -mouse_vel.x
+	#input_vel = mouse_vel
+	
+	if Input.is_action_pressed("kb_right"):
+		input_vel.x -= kb_lean_factor
+	if Input.is_action_pressed("kb_left"):
+		input_vel.x += kb_lean_factor
+	if Input.is_action_pressed("kb_up"):
+		input_vel.y += kb_speed
+	
+	current_lean += input_vel.x * delta * lean_factor
 	if(current_lean >= 0):
 		if(current_lean > max_lean):
 			current_lean = max_lean
@@ -30,7 +46,7 @@ func _process(_delta):
 			if(current_lean < -max_lean):
 				current_lean = -max_lean
 				
-	current_force = mouse_vel.y
+	current_force = input_vel.y
 	if(current_force >= 0):
 		if(current_force > max_force):
 			current_force = max_force
@@ -38,10 +54,12 @@ func _process(_delta):
 		if(current_force < 0):
 			current_force = 0
 			
-
+		
 	update_biker.rpc_id(1, current_lean, current_force)
+	rider.rotation.z = -deg_to_rad(current_lean)
 		
 @rpc("any_peer", "call_local")
 func update_biker(lean, force):
 	current_lean = lean
 	current_force = force
+	rider.rotation.z = -deg_to_rad(current_lean)
